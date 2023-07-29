7bde9d805c110df2942ec89caab863d61d699ebd
VladyslavMalyshenko <koooilrekt@gmail.com>
2023-07-29 17:17:24 +0300

    Made 404 error page more beautiful

--- src/Pages/NoPage/NoPage.jsx
+++ src/Pages/NoPage/NoPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TypeWriterJS from "./nopage";
 
 import "./NoPage.css"
 
@@ -6,7 +7,8 @@ const NoPage = () => {
     return (
         <div className="fzf-error-block">
             <h1 className="fzf-error-text">404</h1>
-            <h5 className="fzf-error-desc">Page not found</h5>
+            <TypeWriterJS></TypeWriterJS>
+            {/*<h5 className="fzf-error-desc">Page not found</h5>*/}
         </div>
     );
 };
