063236e25f34cefdfb1264c9b3d6e941e1cb385c
VladyslavMalyshenko <koooilrekt@gmail.com>
2023-07-31 23:39:18 +0300

    Added axios and also successfully added integration with backend

--- frontend/src/Pages/SingUp/SingUp.jsx
+++ frontend/src/Pages/SingUp/SingUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { AiOutlineEye as Eye, AiOutlineEyeInvisible as EyeInv } from "react-icons/ai";
+import axios from "axios"
 
 import "../../components/styles.css"
 import "./singup.css"
@@ -11,9 +12,56 @@ import Footer from "../../Assets/Footer/Footer";
 
 const SingUp = () => {
     const [showSecretPhraseField, setShowSecretPhraseField] = useState(false);
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [confirm_password, setConfirmPassword] = useState("")
+    const [secret_phrase, setSecretPhrase] = useState("")
     const [passVisible, setPassVisible] = useState(false)
+    const [Error, setError] = useState("")
+    const errorElem = document.querySelector("#error-text")
+
+    function reg_user() {
+        axios.post('http://localhost:8000/auth/registration/', {
+            "username":username,
+            "email":email,
+            "password":password,
+            "password_confirmation":confirm_password
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+                function checkErrorType() {
+                    if (typeof error.response.data.detail === "object") {
+                        setError(error.response.data.detail[0].msg)
+                    } else {
+                        setError(error.response.data.detail)
+                    }
+                }
+                checkErrorType()
+                errorElem.innerHTML = Error
+            });
+    }
+//error.response.data.detail
+    function reg_user_admin() {
+        axios.post('http://localhost:8000/auth/registration/', {
+            "username":username,
+            "email":email,
+            "password":password,
+            "password_confirmation":confirm_password,
+            "secret_phrase": secret_phrase
+        })
+            .then(function (response) {
+                console.log("{ENIS");
+            })
+            .catch(function (error) {
+                console.log(error);
+                setError(error.response.data.detail);
+                errorElem.innerHTML = Error
+            });
+    }
 
     const toggleSecretPhraseField = () => {
         setShowSecretPhraseField(!showSecretPhraseField);
@@ -38,11 +86,11 @@ const SingUp = () => {
                                     <div className="singup-forms-form-left">
                                         <div className="singup-form-block">
                                             <h5>Username</h5>
-                                            <input className="reg-input" placeholder="Please enter an username"/>
+                                            <input value={username} onChange={e => setUsername(e.target.value)} className="reg-input" placeholder="Please enter an username"/>
                                         </div>
                                         <div className="singup-form-block">
                                             <h5>Email</h5>
-                                            <input className="reg-input" placeholder="Please enter an email"/>
+                                            <input value={email} onChange={e => setEmail(e.target.value)} className="reg-input" placeholder="Please enter an email"/>
                                         </div>
                                         <div className="singup-form-block admin-field">
                                             <CSSTransition
@@ -59,7 +107,7 @@ const SingUp = () => {
                                                 classNames="admin-field"
                                                 unmountOnExit
                                             >
-                                                <input className="reg-input" placeholder="Please enter a secret phrase" />
+                                                <input value={secret_phrase} onChange={e => setSecretPhrase(e.target.value)} className="reg-input" placeholder="Please enter a secret phrase" />
                                             </CSSTransition>
                                         </div>
                                     </div>
@@ -102,9 +150,14 @@ const SingUp = () => {
                                     </div>
                                 </div>
 
-                                <button className="form-button-submit">
+                                <button type="button" onClick={
+                                    showSecretPhraseField === true ?
+                                    reg_user_admin :
+                                    reg_user
+                                } className="form-button-submit">
                                     Submit
                                 </button>
+                                <h5 style={{color:"red"}} id="error-text" />
                                 <h5 style={{color:"lightgray", marginTop:"30px"}}>Already have an account? <a className="font-color-link" href="/singin">Sing in!</a></h5>
                             </form>
                         </div>
