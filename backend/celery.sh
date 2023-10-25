168180cba4488604a8c1999d31b2a897613cb9c2
Oleg Kuzmenko <olegkuzmenko2005@gmail.com>
2023-10-25 14:25:22 +0300

    Removed old project

--- backend/celery.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /portfolio/backend/src/settings
-
-if [[ "${1}" == "celery" ]]; then
-  celery --app=tasks.tasks:celery worker -l INFO
-elif [[ "${1}" == "flower" ]]; then
-  celery --app=tasks.tasks:celery flower
-fi
--- backend/src/settings/__init__.py
+++
@@ -1,3 +0,0 @@
-from src.auth import models as auth_models
-from src.portfolio_info import models as info_models
-from src.blog import models as blog_models
